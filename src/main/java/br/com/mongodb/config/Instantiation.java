10486afdae1581a470c064fae4c015fdcb3a0561
Joaquim Francisco Vieira Junior <joaquimjuniorsf@gmail.com>
2020-03-09 14:53:59 -0300

    Criando Entity POST com User aninhado

--- src/main/java/br/com/mongodb/config/Instantiation.java
+++ src/main/java/br/com/mongodb/config/Instantiation.java
@@ -1,12 +1,15 @@
 package br.com.mongodb.config;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import br.com.mongodb.doman.Post;
 import br.com.mongodb.doman.User;
+import br.com.mongodb.repository.PostRepository;
 import br.com.mongodb.repository.UserRepository;
 
 @Configuration
@@ -14,17 +17,27 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userReposiroty;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		
 		userReposiroty.deleteAll();
+		postRepository.deleteAll();
+		
 		
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
+		Post p1 = new Post(null,LocalDate.of(2018, 3, 21),"Partiu viagem","Vou viajar para São Paulo. Abraços", maria);
+		Post p2 = new Post(null,LocalDate.of(2018, 3, 23),"Partiu viagem","Vou viajar para São Paulo. Abraços", alex);
+		
+		
 		userReposiroty.saveAll(Arrays.asList(maria, alex, bob));
+		postRepository.saveAll(Arrays.asList(p1,p2));
 		
 	}
 
---
+++ src/main/java/br/com/mongodb/repository/PostRepository.java
@@ -0,0 +1,12 @@
+package br.com.mongodb.repository;
+
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.mongodb.doman.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
