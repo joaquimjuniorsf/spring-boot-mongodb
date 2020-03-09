e46791f18a6fcbaf077fea5550915ea33ba3365b
Joaquim Francisco Vieira Junior <joaquimjuniorsf@gmail.com>
2020-03-08 22:33:04 -0300

    Conectando ao MongoDB com repository e service

--- src/main/java/br/com/mongodb/resouces/UserResource.java
+++ src/main/java/br/com/mongodb/resouces/UserResource.java
@@ -1,31 +1,29 @@
 package br.com.mongodb.resouces;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.mongodb.doman.User;
+import br.com.mongodb.service.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1","Maria Brown","maria@gmail.com");
 		
-		User alex = new User("2","Alex Green","alex@gmail.com");
+		List<User> list = userService.findAll();
 		
-		List<User> list = new ArrayList<>();
-		
-		list.addAll(Arrays.asList(maria,alex));
-		
-		return list;
+		return ResponseEntity.ok().body(list);
 	}
 
 }
